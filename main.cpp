54cfd349aba7104a2a3d0133f07c25ed5896472e
Fedor <fedor11487@gmail.com>
2019-05-23 01:22:56 +0300

    task tracker

--- main.cpp
+++ main.cpp
@@ -5,36 +5,52 @@
 
 using namespace std;
 
-enum class Lang {
-  DE, FR , IT
-};
-
-struct Region {
-  string std_name;
-  string parent_std_name;
-  map<Lang, string> names;
-  int64_t population;
-};
-
-bool operator==(const Region& Lreg, const Region& Rreg){
-  return tie(Lreg.std_name, Lreg.parent_std_name, Lreg.names, Lreg.population) ==
-         tie(Rreg.std_name, Rreg.parent_std_name, Rreg.names, Rreg.population);
-}
-
-int FindMaxRepetitionCount(const vector<Region>& regions)
+enum class TaskStatus
 {
- // map<Region&, int> mp;
-  int max = 0;
-  for(const auto& x : regions){
-      int count = 0;
-      for( const auto& y : regions){
-          if(x == y){++count;}
-        }
-      if(count > max){max = count;}
-    }
+  NEW,
+  IN_PROGRESS,
+  TESTING,
+  DONE
+};
 
-  return  max;
-}
+struct taskCounter{
+  int n;
+  int inP;
+  int t;
+  int d;
+};
+
+using TaskInfo = map<TaskStatus, int>;
+
+class TeamTasks {
+public:
+  const TaskInfo& GetPersonTasksInfo(const string& person) const{
+    map<string, taskCounter>::iterator it;
+    it = personVocabulary.find(person);
+    return ;
+  }
+
+  void AddNewTask(const string& person){
+    taskCounter newTC{0, 0, 0, 0};
+    personVocabulary.insert(pair<const string&, taskCounter&>(person, newTC)); //newTC copy or no?
+  }
+
+  tuple<TaskInfo, TaskInfo> PerformPersonTasks(const string& person, int task_count){
+
+  }
+
+private:
+  map<string, taskCounter> personVocabulary;
+ // map<string, taskCounter>::iterator it;
+
+  struct TTvoc{
+    string Person;
+    TaskInfo Tsk;
+  };
+};
+
+
+
 
 int main()
 {
